635fd14c2f7fd15abe22a7a9a41ce3b25bc15750
shrayank123 <74706668+shrayank123@users.noreply.github.com>
2021-01-27 20:40:07 +0700

    Add files via upload

--- Ball.js
+++ Ball.js
@@ -1,11 +1,12 @@
-class Box {
+class Ball {
     constructor(x, y, width, height) {
       var options = {
           'restitution':0.8,
-          'friction':1.0,
+          'friction':0.3,
           'density':0.04,
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
+      this.image = loadImage("sprites/polygon.png");
       this.width = width;
       this.height = height;
       
@@ -17,11 +18,11 @@ class Box {
       push();
       translate(pos.x, pos.y);
       rotate(angle);
-      rectMode(CENTER);
-      strokeWeight(4);
-      stroke("blue");
-      fill("red");
-      rect(0, 0, this.width, this.height);
+      fill("yellow");
+
+      imageMode(CENTER)
+        //ellipse(0,0,this.r,this.r)
+        image(this.image,0,0,this.width,this.height)
       pop();
     }
   }
--- sketch.js
+++ sketch.js
@@ -1,70 +1,78 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var engine, world;
 
 
 function preload(){
-
-binImg = loadImage("sprites/dustbin.png")
-
-}
-
-
-
-
-function setup() {
-	var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
-
-	 Engine.run(engine)
-  //Create the Bodies Here.
-
-  bin = createSprite(909,315,20,20);
-  bin.addImage(binImg);
-  bin.scale = 0.45;
-
-	
-	box1 = new Box(860,310,10,140);
-	box2 = new Box(954,310,10,140);
-	box3= new Box(907,390,80,20);
-	
-	ground = new Ground(600,height,1200,20)
-
-	paper1 = new Paper(200,200,40)
-  
-}
-
-
-function draw() {
-  rectMode(CENTER);
-  background("blue");
-
-  Engine.update(engine);
-
-   
-  box1.display();
-  box2.display();
-  box3.display();
-  ground.display();
-  paper1.display();
-
-
-  drawSprites();
  
 }
 
-function keyPressed()
-{
+function setup() {
+  var canvas = createCanvas(1200,400);
+  engine  = Engine.create();
+  world = engine.world;
+ 
+  ground1=new Ground(500,400,1200,30)
 
-  if(keyCode === UP_ARROW)
-  {
-    Matter.Body.applyForce(paper1.body,paper1.body.position,{x:90,y:-100})
-  }
+   
+    box1 = new Box(400,350,50,50)
+    box2 = new Box(450,350,50,50)
+    box3 = new Box(500,350,50,50)
+    box4 = new Box(550,350,50,50)
+    box5 = new Box(600,350,50,50)
+    box6 = new Box(650,350,50,50)
+    box7 = new Box(450,300,50,50)
+    box8 = new Box(500,300,50,50)
+    box9 = new Box(550,350,50,50)
+    box10 = new Box(600,350,50,50)
+    box11 = new Box(500,350,50,50)
+    box12 = new Box(550,350,50,50)
+   
+    ball1 = new Ball(100,200,40,40)
+
+    rope1= new Rope(ball1.body,{x:180,y:90})
 
 }
 
+function draw() {
+ // background(backgroundImg);  
+ background("lightblue")
+  Engine.update(engine);
+  ground1.display()
+  
+  
+  
+  box1.display()
+  box2.display()
+  box3.display()
+  box4.display()
+  box5.display()
+  box6.display()
+  box7.display()
+  box8.display()
+  box9.display()
+  box10.display()
+  box11.display()
+  box12.display()
+
+  ball1.display()
+
+  rope1.display()
+}
+  
+
+function mouseDragged(){
+    Matter.Body.setPosition(ball1.body,{x:mouseX,y:mouseY});
+  }
+  
+ 
+  function mouseReleased(){
+    rope1.fly();
+  }
+  
+ // ground.display();
+  
 
 
